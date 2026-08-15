79973d421600bc5d32b9560ede8fcb0be12bd950
romain5276p-sys <romain5276p@gmail.com>
2026-08-15 19:13:31 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyC_vcm4r4gmX8Y9pfSJeq2YHLpH3XTXGM0",
